b3c4ce24e8bedf1eb89336908e91da2caffcd629
Pip3r4o <pip3r4o@gmail.com>
2015-12-05 14:37:23 +0200

    updated script

--- texasHoldem.js
+++ texasHoldem.js
@@ -1,39 +1,48 @@
-var getResult = (function () {
-    var name = document.getElementsByTagName("h1")[0].innerText;
+var getPokerResults = (function () {
+	var name = document.getElementsByTagName("h1")[0].innerText;
 
-    var table = document.getElementsByTagName("table")[0].getElementsByTagName("tbody")[0].children;
+	var table = document.getElementsByTagName("table")[0].getElementsByTagName("tbody")[0].children;
 
-    var result = {};
-    result["First"] = {};
-    result["First"].GamesWon = 0;
-    result["First"].HandsPlayed = 0;
+	var result = {};
+	result["First"] = {};
+	result["First"].GamesWon = 0;
+	result["First"].HandsPlayed = 0;
 
-    result["Second"] = {};
-    result["Second"].GamesWon = 0;
-    result["Second"].HandsPlayed = 0;
+	result["Second"] = {};
+	result["Second"].GamesWon = 0;
+	result["Second"].HandsPlayed = 0;
 
-    result["Name"] = name;
-    
-    for (var i = 1; i < table.length; i++) {
-        var cols =  table[i].getElementsByTagName("td");
-        var winner = cols[1].innerText;
+	result["Name"] = name;
 
-        var cards = cols[2].innerText;
-        cards = cards.match(/\W+[0-9]+/)[0].trim();
+	for (var i = 1; i < table.length; i++) {
+		var cols =  table[i].getElementsByTagName("td");
+		var winner = cols[1].innerText;
 
-        if (winner === "First") {
-            result["First"].HandsPlayed += cards * 1;
-            result["First"].GamesWon += 1;
-        }
-        else
-        {
-            result["Second"].HandsPlayed += cards * 1;
-            result["Second"].GamesWon += 1;
-        }
-    }
+		var cards = cols[2].innerText;
+		cards = cards.match(/\W+[0-9]+/)[0].trim();
 
-    result["First"].AverageHandsPer = result["First"].HandsPlayed / result["First"].GamesWon;
-    result["Second"].AverageHandsPer = result["Second"].HandsPlayed / result["Second"].GamesWon;
+		if (winner === "First") {
+			result["First"].HandsPlayed += cards * 1;
+			result["First"].GamesWon += 1;
+		}
+		else
+		{
+			result["Second"].HandsPlayed += cards * 1;
+			result["Second"].GamesWon += 1;
+		}
+	}
 
-    return result;
-}());
+	result["First"].AverageHandsPer = result["First"].HandsPlayed / result["First"].GamesWon;
+	result["Second"].AverageHandsPer = result["Second"].HandsPlayed / result["Second"].GamesWon;
+
+	console.log(result.Name);
+	console.log("Games won by First: " + result.First.GamesWon);
+	console.log("Hands played when won by First: " + result.First.HandsPlayed);
+	console.log("Average hands played when won by First: " + result.First.AverageHandsPer.toFixed(2));
+	console.log("---------------------------------------")
+	console.log("Games won by Second: " + result.Second.GamesWon);
+	console.log("Hands played when won by Second: " + result.Second.HandsPlayed);
+	console.log("Average hands played when won by Second: " + result.Second.AverageHandsPer.toFixed(2));
+
+	return result;
+})();
